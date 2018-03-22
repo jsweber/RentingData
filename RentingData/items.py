03b78994634ab823ad2b05f2113c869885d66db6
jsweber <951092973@qq.com>
2018-03-23 01:18:02 +0800

    add item

--- RentingData/items.py
+++ RentingData/items.py
@@ -12,3 +12,20 @@ class RentingdataItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class LianjiaItem(scrapy.Item):
+    id = scrapy.Field() #url => md5
+    url = scrap.Field()
+    main_title = scrapy.Field()
+    price = scrapy.Field()
+    price_unit = scrapy.Filed()  #价格单位
+    size = scrapy.Field() #房子尺寸
+    floor = scrapy.Field() #楼层
+    house_type = scrapy.Field() #户型
+    house_orientation = scrapy.Field() #房屋朝向
+    subway = scrapy.Field() 
+    community = scrapy.Field() #小区
+    location = scrapy.Field() #位置
+    publish_time = scrapy.Field() #房屋发布时间
+    contact = scrapy.Field()
+    contact_man = scrapy.Field() #联系人    
