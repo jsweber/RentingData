f68ecedfb934d29180c03af9a58d9fc47f7f45a0
jsweber <951092973@qq.com>
2018-04-07 23:26:30 +0800

    完成上海地区数据的全量抓取

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(['scrapy', 'crawl', 'lianjia'])
+# execute(['scrapy', 'crawl', 'lianjia'])
+execute(['scrapy', 'crawl', 'lianjia2'])
